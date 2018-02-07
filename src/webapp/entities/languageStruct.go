5856ff2c3d915e638bd149325bee40b67ff569d5
l-gu <laurent.guer1@gmail.com>
2018-02-07 14:43:17 +0100

    Language - ToString()

--- src/webapp/entities/languageStruct.go
+++ src/webapp/entities/languageStruct.go
@@ -1,7 +1,15 @@
 package entities
 
+import "fmt"
+
 type Language struct {
         Code  string
         Name  string
 }
 
+func (this Language) ToString() string {
+    return fmt.Sprintf(
+    	"[%s, %s]", 
+    	this.Code, 
+    	this.Name )
+}
